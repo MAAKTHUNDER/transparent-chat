3f114609666d824caf2002b5afbb0ab09f4f4014
MAAKTHUNDER <ahmad_ali.khan@yahoo.com>
2025-09-19 22:10:16 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,47 @@
 const { app, BrowserWindow, globalShortcut } = require('electron');
+const Store = require('electron-store');
 
 let win;
-let locked = true;  start click-through by default
+let locked = true; // start click-through by default
+const store = new Store();
 
 function createWindow() {
+  // Load previous bounds or use defaults
+  const bounds = store.get('windowBounds') || { width: 400, height: 600 };
+
   win = new BrowserWindow({
-    width 400,
-    height 600,
-    frame false,
-    transparent true,
-    alwaysOnTop true,
-    resizable true,
-    skipTaskbar true,
-    webPreferences {
-      nodeIntegration false,
+    ...bounds, // spread previous x,y,width,height
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    resizable: true,
+    skipTaskbar: true,
+    webPreferences: {
+      nodeIntegration: false,
     }
   });
 
-  win.loadURL(httpsmultichat.livepush.iomcSprPsAwsSs0D2XU);
+  // Load your chat URL
+  win.loadURL("https://multichat.livepush.io/mcSprPsAwsSs0D2XU");
 
-   Overlay starts click-through
+  // Overlay starts click-through
   win.setIgnoreMouseEvents(true);
 
-   Toggle lockunlock with F10
-  globalShortcut.register('F10', () = {
+  // Save bounds when window is moved/resized
+  win.on('close', () => {
+    store.set('windowBounds', win.getBounds());
+  });
+
+  // Toggle lock/unlock with F10
+  globalShortcut.register('F10', () => {
     locked = !locked;
     win.setIgnoreMouseEvents(locked);
-    console.log(Overlay locked, locked);
+    console.log("Overlay locked:", locked);
   });
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () = {
+app.on('window-all-closed', () => {
   app.quit();
 });
